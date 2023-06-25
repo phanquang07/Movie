a0af1159bfaa27394a52132defd742e8280bd820
Phan Quang <quangphan0724@gmail.com>
2023-06-25 23:10:42 +0700

    fix character spelling import

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { getApiConfig, getGenres } from "./store/homeSlice";
 import { fetchDataFromApi } from "./utils/api";
-import Header from "./components/header/header";
+import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
 import Details from "./pages/details/Details";
